50ac3c2822596198ee5e65614f1a4121f3b7621f
femerkler <81877280+femerkler@users.noreply.github.com>
2021-05-26 18:56:51 -0300

    Add files via upload

---
+++ APS_Tema_2/Main.java
@@ -0,0 +1,17 @@
+package APS_Tema_2;
+
+import java.util.ArrayList;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        Produto e = new Eletronico(110, 250, "A", 1, "Geladeira", "Eletrolux", 10, 1000);
+        
+        e.vender(1);
+        
+        ArrayList<Produto> produtos = new ArrayList();
+        
+    }
+    
+}
